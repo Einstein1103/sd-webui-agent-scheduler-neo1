066063eb2eda1b3e5b600ed2236d5fd176abd0b4
Einstein1103 <62929210+Einstein1103@users.noreply.github.com>
2026-07-18 11:27:55 +0800

    Update task_runner.py

--- agent_scheduler/task_runner.py
+++ agent_scheduler/task_runner.py
@@ -503,19 +503,6 @@ class TaskRunner:
             log.info("[AgentScheduler] Runner is paused")
             return None
 
-        # # delete task that are too old
-        # retention_days = 30
-        # if (
-        #     getattr(shared.opts, "queue_history_retention_days", None)
-        #     and shared.opts.queue_history_retention_days in task_history_retenion_map
-        # ):
-        #     retention_days = task_history_retenion_map[shared.opts.queue_history_retention_days]
-
-        # if retention_days > 0:
-        #     deleted_rows = task_manager.delete_tasks(before=datetime.now() - timedelta(days=retention_days))
-        #     if deleted_rows > 0:
-        #         log.debug(f"[AgentScheduler] Deleted {deleted_rows} tasks older than {retention_days} days")
-
         self.__total_pending_tasks = task_manager.count_tasks(status="pending")
 
         # get more task if needed
